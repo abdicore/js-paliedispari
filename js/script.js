198ee72275ca520aeb46d4342705213c5de93be8
abdicore <abdirhamanmohamed@gmail.com>
2024-08-24 21:46:07 +0200

    reverse-word

--- js/script.js
+++ js/script.js
@@ -1,37 +1,32 @@
 /*
-Palidroma
+Palindroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// Chiedere all’utente di inserire una parola
-
-
 //FASE DI PREPARAZIONE
 //dichiarazioni funzioni
-function isPalindrome(word){
-    //capovolgo la parola
-    if (word.revers() === word){
-        return console.log(palindroma)}
-    //contollo se la parola e uguare
+function isPalindrome(word) {
+    // Trasformo la parola in un array di caratteri, capovolgo l'array e lo unisco in una stringa
+    let wordReverse = word.split('').reverse().join('');
+    // log parola al rovsci
+    return console.log(wordReverse);
 }
 
 //RACCOLGO GLI ELEMENTI DAL DOM
-
-const form = document.getElementById('palindroma-form')
-const imputField = document.getElementById('inputText')
-const resultElement =  document.getElementById('result')
+const form = document.getElementById('palindroma-form');
+const inputField = document.getElementById('inputText');  // correggi 'imputField' in 'inputField'
+const resultElement = document.getElementById('result');
 
 form.addEventListener('submit', function(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     //raccolta dati
-    const word = imputField.value.trim()
+    const word = inputField.value.trim();  // usa 'inputField' corretto
 
     //validazione
+ 
 
     //fase elaborazione
-    isPalindrome()
-})
-
-
+isPalindrome(word)
+});
